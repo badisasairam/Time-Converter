8b149a1bb772a7157e0b73481b473bc5117bd4a6
Badisa,Sairam <s534148@ch140008m.nwmissouri.edu>
2019-02-05 09:48:09 -0600

    Initial Commit

---
+++ Time Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time Converter
+//
+//  Created by Badisa,Sairam on 2/5/19.
+//  Copyright © 2019 Badisa,Sairam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
